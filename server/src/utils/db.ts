e630c96c136cb125122f464fabd8ab2189c164f2
unknown <eexdhe@gmail.com>
2021-04-22 12:22:40 +0200

    change db connect file

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -6,19 +6,12 @@ if((process.env.NODE_ENV!) === "production"){
   url = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PWD}@cluster0.51prx.mongodb.net/athena?retryWrites=true&w=majority`
 }
 
-const connect = async () => {
-  try{
-    await mongoose.connect(url, {
+ mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true
     });
-  }catch(e){
-    console.log(e)
-  }
-};
 
-connect();
 
 
