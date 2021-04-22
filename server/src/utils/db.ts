ad149ca8b4b8a062e1428968aaf453706aecd608
unknown <eexdhe@gmail.com>
2021-04-22 12:45:12 +0200

    put mongoose connect inside a function

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -1,17 +1,26 @@
 import mongoose from "mongoose";
 
-
 let url = "mongodb://127.0.0.1:27017/athena"
 if((process.env.NODE_ENV!) === "production"){
   url = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PWD}@cluster0.51prx.mongodb.net/athena?retryWrites=true&w=majority`
 }
 
- mongoose.connect(url, {
+const connect = async () => {
+  try{
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true
     });
+  }catch(e) {
+    console.log(e)
+  }
+}
+
+connect();
+
+ 
 
 
 
